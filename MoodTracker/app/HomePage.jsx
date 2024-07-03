dca02a652c16c6475e47099fdcfd0dd5b064944a
aakashkbw <114473332+aakashkbw@users.noreply.github.com>
2024-07-03 16:32:29 +0200

    Emojis sind klickbar

--- MoodTracker/app/HomePage.jsx
+++ MoodTracker/app/HomePage.jsx
@@ -1,19 +1,22 @@
 import React from "react";
-import { View, Text, Image, Pressable } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import EmojiImage from "../components/EmojiImage";
+
 export default function Home() {
   const [emojiPage, setEmojiPage] = React.useState(true);
   const [selfiePage, setSelfiePage] = React.useState(false);
-  const [resutlPage, seetResultPage] = React.useState(false);
+  const [resultPage, setResultPage] = React.useState(false);
+  const [selectedEmoji, setSelectedEmoji] = React.useState(null);
 
-  const completeStep1 = () => {
+  const completeStep1 = (emoji) => {
+    setSelectedEmoji(emoji);
     setEmojiPage(false);
     setSelfiePage(true);
   };
-  
+
   const completeStep2 = () => {
     setSelfiePage(false);
-    seetResultPage(true);
+    setResultPage(true);
   };
 
   return (
@@ -21,24 +24,38 @@ export default function Home() {
       <Text className="text-white text-3xl font-bold text-center">SMILE</Text>
       {emojiPage && (
         <View>
-          <View className=" flex flex-row w-full justify-between items-center px-4 mt-10 ">
+          <View className="flex flex-row w-full justify-between items-center px-4 mt-10">
             <EmojiImage
               src={require("../Emojis/dropFace.png")}
-              onPress={() => console.log("laugh")}
+              onPress={() => completeStep1("dropFace")}
             />
             <EmojiImage
               src={require("../Emojis/lessSad.png")}
-              onPress={() => console.log("laugh")}
+              onPress={() => completeStep1("lessSad")}
             />
             <EmojiImage
               src={require("../Emojis/happy.png")}
-              onPress={() => console.log("sad")}
+              onPress={() => completeStep1("happy")}
             />
             <EmojiImage
               src={require("../Emojis/Happier.jpeg")}
-              onPress={() => console.log("cry")}
+              onPress={() => completeStep1("happier")}
             />
           </View>
+        </View>
+      )}
+      {selfiePage && (
+        <View>
+          <Text className="text-white text-3xl">Take a Selfie</Text>
+          <Pressable onPress={completeStep2}>
+            <Text className="text-white text-xl">Next</Text>
+          </Pressable>
+        </View>
+      )}
+      {resultPage && (
+        <View>
+          <Text className="text-white text-3xl">Result Page</Text>
+          <Text className="text-white text-xl">Selected Emoji: {selectedEmoji}</Text>
         </View>
       )}
     </View>
